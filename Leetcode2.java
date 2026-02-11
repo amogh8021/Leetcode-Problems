fd453837c20f573d8ee268f0eb155dc2319c8879
amogh8021 <amoghdev8021@gmail.com>
2026-02-12 00:16:51 +0530

    second question solved

---
+++ Leetcode2.java
@@ -0,0 +1,54 @@
+public class Leetcode2 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int[] output = new int[nums.length];
+        int k = nums.length - 1;
+
+        while (left <= right) {
+
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                output[k] = nums[left] * nums[left];
+                left++;
+            } else {
+                output[k] = nums[right] * nums[right];
+                right--;
+            }
+
+            k--;
+        }
+
+        return output;
+    }
+}
+
+
+//https://leetcode.com/problems/squares-of-a-sorted-array/
+//
+//Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [-4,-1,0,3,10]
+//Output: [0,1,9,16,100]
+//Explanation: After squaring, the array becomes [16,1,0,9,100].
+//        After sorting, it becomes [0,1,9,16,100].
+//Example 2:
+//
+//Input: nums = [-7,-3,2,3,11]
+//Output: [4,9,9,49,121]
+//
+//
+//Constraints:
+//
+//        1 <= nums.length <= 104
+//        -104 <= nums[i] <= 104
+//nums is sorted in non-decreasing order.
+
